cdb9c5150968cba2b48976b75d06862a15f6bb59
Alex Piers <apiers@uw.edu>
2018-11-28 16:12:19 -0800

    change - add more images to find muon tracks in

--- muonAnalysis/src/SetChain.C
+++ muonAnalysis/src/SetChain.C
@@ -4,7 +4,7 @@ gROOT->ProcessLine(".L ~/recon/Viewer.C+");
 gROOT->ProcessLine(".L ~/recon/ADPmacros/muonFilter.c");
 
 TChain* c = new TChain("clusters_tree");
-c->Add("/gscratch/damic/data/uchicago/processed/D3500/SbBe_2015-04-10_FullBe/root/Image176*.root");
+c->Add("/gscratch/damic/data/uchicago/processed/D3500/SbBe_2015-04-10_FullBe/root/Image1*.root");
 
 c->SetAlias("ene","charge_total.fVal/10300*6.4");
 c->SetAlias("x","charge_mean_x.fVal");
