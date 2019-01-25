168645b24ccd2192c134bcc6c63b86e76a7e989e
Alexander Piers <apiers@uw.edu>
2019-01-24 21:51:05 -0800

    fix - issues with plotting the a histogram over sigma max

--- muonAnalysis/plots/plotDiffusionParameters.cxx
+++ muonAnalysis/plots/plotDiffusionParameters.cxx
@@ -1,33 +1,57 @@
-void plotDiffusionParameters(char *infile, double ANeutron=220, double bNeutron=5.6E-4){
+void plotDiffusionParameters(double ANeutron=220, double bNeutron=5.6E-4){
 
 	const double zd = 500;
 	gStyle->SetOptFit(1);
+	string infile = "../../rootfiles/likelihoodFit/muonTree15kev_0999ccf_fits.root";
+	TFile *f = new TFile(infile.c_str());
+	TTree *t = (TTree*)f->Get("muonFit");
 
-	TFile *f = new TFile(infile);
-	TH1D *sigmamax = (TH1D*)f->Get("sigmamax");
-	TH1D *a = (TH1D*)f->Get("a");
+
+	// Create and fill histograms from tree data
+	TH1D *sigmamax = new TH1D("sigmamax","#sigma_{max}", 200, 0, 10);
+	TH1D *a = new TH1D("a", "a", 100, -10, 10);
+
+	double smaxVal, aVal;
+	t->SetBranchAddress("sigmamax", &smaxVal);
+	t->SetBranchAddress("a", &aVal);
+
+	for(int i=0; i<t->GetEntries(); i++){
+		t->GetEntry(i);
+		sigmamax->Fill(smaxVal);
+		a->Fill(aVal);
+	}
 
 	// Plot sigma max and fit
+	TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
 	sigmamax->SetLineWidth(2);
 	TF1 *sigmamaxFit = new TF1("sigmamaxFit", "gaus", 7.5, 10);
+	// TF1 *sigmamaxFit = new TF1("sigmamaxFit", "[0]*TMath::Exp(-[1])*TMath::Power([1],x)/TMath::Gamma(x+1)", 0, 10);
+	// sigmamaxFit->SetParameter(1, 8.5);
+	// sigmamaxFit->SetParameter(0, 260);
 	sigmamax->Fit("sigmamaxFit", "QIR");
 
-	TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+	
 	sigmamax->Draw();
 	sigmamaxFit->Draw("same");
 
 	// Plot a and fit
-	// a->SetLineWidth(2);
-	TF1 *fA = new TF1("aFit", "gaus", -3.5, 1.5);
-	a->Fit("aFit", "QIR");
 	TCanvas *c2 = new TCanvas("c2", "c2", 800, 600);
+	a->SetLineWidth(2);
+	TF1 *fA = new TF1("aFit", "gaus", -3.5, 1.5);
+	// TF1 *fA = new TF1("aFit", "[0]*TMath::Exp(-[1])*TMath::Power([1],-x)/TMath::Gamma(TMath::Abs(x)+1)", -3, 0);
+	// fA->SetParameter(1, 0.9);
+	// fA->SetParameter(0, 200);
+	a->Fit("aFit", "QIR");
+	
 	c1->cd(2);
 	a->Draw();
-	aFit->Draw("same");
+	fA->Draw("same");
+	cout << sigmamaxFit->GetParameter(1) << endl;
+	cout << fA->GetParameter(1) << endl;
 	
 	// Compute diffusion parameters
-	double b = exp(fA->GetParameter(1)) / zd;
-	double A = -pow(sigmamaxFit->GetParameter(1), 2) / log(1 - b * zd);
+	double b = TMath::Exp(-fA->GetParameter(1)) / zd;
+	double A = -TMath::Power(sigmamaxFit->GetParameter(1), 2) / TMath::Log(1 - b * zd);
 	cout << "A: " << A << endl;
 	cout << "b: " << b << endl;
 
